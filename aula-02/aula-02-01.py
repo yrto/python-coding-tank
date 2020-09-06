dd80181b6f8490295d97b6a5da7a19c84cf976f3
yrto <ayrton.mugnaini@gmail.com>
2020-09-05 23:00:01 -0300

    first files

---
+++ aula-02/aula-02-01.py
@@ -0,0 +1,7 @@
+# Escreva um script que dado o raio de um círculo, calcule a sua circunferência.
+
+raio = float(input("Digite o raio do círculo: "))
+pi = 3.14
+calculo = raio * pi * 2
+
+print(calculo)
---
+++ aula-02/aula-02-02.py
@@ -0,0 +1,1 @@
+# Faça um script que, dado a posição inicial (Si), tempo (t) e velocidade (v), calcule a posição final de um automóvel percorrendo um caminho em linha reta a partir da fórmula S = Si + vt.
