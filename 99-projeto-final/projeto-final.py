476d9c28c2e0f8fd7429bc3c0be29a59b8de28a4
yrto <ayrton.mugnaini@gmail.com>
2020-09-22 21:53:42 -0300

    new exercises

--- 99-projeto-final/projeto-final.py
+++ 99-projeto-final/projeto-final.py
@@ -1,6 +1,18 @@
 # crie um script para gerenciar uma lista de tarefas
 
-# visualizar tarefas
-# adicionar tarefas
-# atualizar tarefas
-# remover tarefas
+# Bem-vindo ao PyTodo
+
+# 1. visualizar tarefas
+# 2. adicionar tarefas
+# 3. atualizar tarefas
+# 4. remover tarefas
+# 0. fechar
+
+# Escolha sua opção:
+
+# ---
+
+Digite o número da tarefa:  # verificar se existe com "in" ou não pedir o número
+Descrição:
+Data:
+Categoria:
